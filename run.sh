479e0effa57e92bd40ff0826e9c42a5473741323
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-22 10:16:16 +0200

    release by maven

--- run.sh
+++ run.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 
+
+function runGithubCi(){
+    ./mvnw -B -DskipTests clean dependency:list install --file pom.xml
+}
+
+function makeRelease() {
+    ./mvnw -DskipTests -Dresume=false -DdryRun=true release:prepare
+    ./mvnw -DskipTests -Dresume=false release:prepare release:perform
+}
+
+function publishSite() {
+    ./mvnw clean install site site:stage scm-publish:publish-scm
+}
+
+
 function site() {
   ./mvnw -e clean install site site:deploy
 }
